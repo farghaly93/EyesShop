a00c78c015b093b0ad9f0ec3fd598f8d9f78f803
farghaly <miserable.farghaly93@gmail.com>
2019-05-23 00:14:27 +0200

    s3

--- app.js
+++ app.js
@@ -7,6 +7,35 @@ const usersRouter = require('./routers/users');
 const adminUsersRouter = require('./routers/adminUsers');
 const mongoose= require('mongoose');
 const app= express();
+const AWS = require('aws-sdk');
+const Busboy = require('busboy');
+
+const BUCKET_NAME = 'eyesshop-front';
+const IAM_USER_KEY = 'AKIAIHQIW67EGZY2BYCA';
+const IAM_USER_SECRET = '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';
+
+function uploadToS3(file) {
+  let s3bucket = new AWS.S3({
+    accessKeyId: IAM_USER_KEY,
+    secretAccessKey: IAM_USER_SECRET,
+    Bucket: BUCKET_NAME
+  });
+  s3bucket.createBucket(function () {
+      var params = {
+        Bucket: BUCKET_NAME,
+        Key: file.name,
+        Body: file.data
+      };
+      s3bucket.upload(params, function (err, data) {
+        if (err) {
+          console.log('error in callback');
+          console.log(err);
+        }
+        console.log('success');
+        console.log(data);
+      });
+  });
+}
 
 mongoose.connect(process.env.mongodb)
 .then(() => {

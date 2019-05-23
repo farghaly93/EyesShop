6a7002fb8f2b4436c63eb0ea35be9b063dc815fb
farghaly <miserable.farghaly93@gmail.com>
2019-05-23 06:22:40 +0200

    update

--- middlewares/photo1upload.js
+++ middlewares/photo1upload.js
@@ -23,9 +23,8 @@ const storage = s3({
   },
   bucket: 'eyesshop-bucket',
     region: 'us-east-2',
-    config = new AWS.Config({
-      accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY, region: 'us-east-2'
-    })
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
 module.exports =  multer({storage: storage}).array('images');

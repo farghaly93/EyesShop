5230d53e43058358cce9736fd371da9aa3011c52
farghaly <miserable.farghaly93@gmail.com>
2019-05-29 05:28:37 +0200

    s3

--- middlewares/photo1upload.js
+++ middlewares/photo1upload.js
@@ -1,25 +1,28 @@
-var AWS = require('aws-sdk');
-var s3 = new AWS.S3();
+var AWS = require('aws-sdk'),
+    fs = require('fs');
 
-// Bucket names must be unique across all S3 users
+// For dev purposes only
+AWS.config.update({ accessKeyId: process.env.AWS_ACCESS_KEY_ID, secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY });
 
-var myBucket = 'eyesshop-bucket';
+// Read in the file, convert it to base64, store to S3
+fs.readFile('hahahaha.jpg', function (err, data) {
+  if (err) { throw err; }
 
-var myKey = 'textFile';
+  var base64data = new Buffer(data, 'binary');
 
+  var s3 = new AWS.S3();
+  s3.client.putObject({
+    Bucket: 'eyesshop-bucket',
+    Key: 'hahahaha.jpg',
+    Body: base64data,
+    ACL: 'public-read'
+  },function (resp) {
+    console.log(arguments);
+    console.log('Successfully uploaded package.');
+  });
 
- params = {Bucket: myBucket, Key: myKey, Body: 'Valueeee or some data' };
-
-     s3.putObject(params, function(err, data) {
-
-         if (err) {
-
-             console.log(err)
-
-         } else {
-
-             console.log("Successfully uploaded data to myBucket/myKey");
-
-         }
-
-      });
+});
+  //bucket: 'eyesshop-bucket',
+   // region: 'us-east-2',
+    //aws_access_key_id: process.env.AWS_ACCESS_KEY_ID,
+    //aws_secret_access_key: process.env.AWS_SECRET_ACCESS_KEY

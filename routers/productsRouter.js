6670b2f87c0036bf90969bb640a07ee644f6341b
farghaly <miserable.farghaly93@gmail.com>
2019-05-29 06:38:46 +0200

    s3

--- routers/productsRouter.js
+++ routers/productsRouter.js
@@ -11,7 +11,7 @@ aws.config.update({
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
     signatureVersion: 'v4',
-    region: 'us-east-2',
+    region: 'us-west-2',
   });
   
   const s3 = new aws.S3();
